48a6ce8d73ced79a64962acec0e4b3dcb42f32e5
Martin Vassor <martin.vassor@cs.ox.ac.uk>
2024-03-22 17:19:05 +0000

    Initial version of the script

---
+++ install_rumpsteak_morello_toolchain.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+set -x
+
+init() {
+	clear
+}
+
+get_install_path() {
+	echo "Enter the install path of the toolchain
+A directory 'rumpsteak_morello' will be created there, containing all components
+of the toolchain"
+	read -p "PATH? " INSTALL_DIR
+	INSTALL_DIR="$(realpath "$INSTALL_DIR")/rumpsteak_morello"
+	echo "install in $INSTALL_DIR"
+	INIT_PATH="$PWD"
+}
+
+#1 path
+create_install_dir() {
+	if test -f "$1"
+	then
+		echo "$1 is a file"
+		false
+	else
+		echo "Creating directory $1"
+		mkdir -p "$1"
+		true
+	fi
+}
+
+install_futures() {
+	cd "$INSTALL_DIR"
+	git clone -b token-bilockimpl https://github.com/exrook/futures-rs.git
+	cd "$INIT_PATH"
+}
+
+install_rumpsteak() {
+	cd "$INSTALL_DIR"
+	git clone https://github.com/zakcutner/rumpsteak
+	cd rumpsteak
+	echo "$PWD"
+	cat "../../rumpsteak.patch"
+	patch Cargo.toml < ../../rumpsteak.patch
+	cd "$INIT_PATH"
+}
+
+#1 install path
+prepare_cargo_patch() {
+	# Assumes the path does *not* contains "=", might cause issues
+	sed "s=INSTALL_DIR=$INSTALL_DIR=" Cargo.toml.patch.template > Cargo.toml.patch
+}
+
+init
+get_install_path
+create_install_dir "$INSTALL_DIR" || (echo "abort install"; exit -1)
+install_futures
+install_rumpsteak
+prepare_cargo_patch
